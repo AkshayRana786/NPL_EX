4089990838eb989ba6d2d1b9a6e6430a8e4e25ce
AkshayRana786 <65900213+AkshayRana786@users.noreply.github.com>
2020-06-15 15:12:38 +0530

    Commit done

---
+++ NLP.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+# Importing the dataset
+dataset = pd.read_csv("Restaurant_Reviews.tsv", delimiter="\t", quoting=3)
+dataset.head()
+
+
+# In[3]:
+
+
+#Cleaning the texts
+import re
+import nltk
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+N = dataset.shape[0]
+
+for i in range(0, N):
+    review = re.sub('[^A-Za-z]',' ',dataset["Review"][i])
+    review = review.lower() 
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+corpus
+
+
+# In[4]:
+
+
+# Creating the Beg of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[: , 1].values
+
+
+# In[5]:
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# In[6]:
+
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+
+# In[7]:
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+
+# In[9]:
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+cm
+
+
+# In[10]:
+
+
+(55 + 91)/200
+
